55260f14d4fec8e02e39bf8b74917e3d1a12655b
krishna saini <krishnapraanjal10@gmail.com>
2023-10-05 13:58:32 +0530

    add nav bar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,58 @@
+// components/NavBar.js
+
+import Link from "next/link";
+import styles from "./NavBar.module.css"; // Import the CSS module
+import { signIn, signOut, useSession } from "next-auth/react";
+
+function NavBar() {
+  //   const router = useRouter();
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (e.target.textContent === "Sign In") {
+      signIn("google");
+    } else {
+      signOut();
+    }
+  };
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href="/" className={styles.navLink}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/dashboard" className={styles.navLink}>
+            Dashboard
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/cars" className={styles.navLink}>
+            All Cars
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link
+            href="/api/auth/signin"
+            className={styles.navLink}
+            onClick={() => handleClick}
+          >
+            Sign In
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link
+            href="/api/auth/signout"
+            className={styles.navLink}
+            onClick={() => handleClick}
+          >
+            Sign Out
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
