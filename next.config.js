365352687431b469d7d7db77d99dc2a26dd20e30
krishna saini <krishnapraanjal10@gmail.com>
2023-10-05 13:05:00 +0530

    set up next auth and google login mechanism

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    GOOGLE_CLIENT_ID:
+      "823218194039-t4trtqn3b0v29omnhf6btmae41sep4s5.apps.googleusercontent.com",
+      GOOGLE_CLIENT_SECRET: "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",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,12 @@
+import nextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
